f970c4300148893b11fff1c10562cf67c6e913c5
Isaac Hookom <isaac.hookom@cequence.ai>
2024-08-08 14:31:53 -0400

    messing around with lego

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import gollum from './assets/gollum.jpeg';
 import './App.css';
+import { instructions } from './instructions';
+import { Dialog, Typography, Paper } from '@mui/material';
 
-function App() {
+const App = () => {
+  const [search, setSearch] = useState('');
+  const [linkTarget, setLinkTarget] = useState();
+  const [multiLinkModalTarget, setMultiLinkModalTarget] = useState();
+
+  const openLinks = (links) => {
+    if (links.length < 1) return;
+    else if (links.length === 1) {
+      setLinkTarget(links[0]);
+    } else {
+      setMultiLinkModalTarget(links)
+    }
+  };
+
+  const rows = instructions.filter(i => {
+    const standardizedSearch = search.toLowerCase();
+    if (!standardizedSearch ||
+        i.name.toLowerCase().includes(standardizedSearch) ||
+        i.number.toString().includes(standardizedSearch) ||
+        i.theme.toLowerCase().includes(standardizedSearch)) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='container'>
+      <header className='appHeader'>
+        <img src={gollum} alt='logo' />
+        <p>Lego Map Search</p>
       </header>
+      <div className='body'>
+        <input
+          className='searchInput'
+          value={search}
+          placeholder='Search'
+          onChange={e => setSearch(e.target.value)}
+        />
+        <table className='mapList'>
+          <thead>
+            <tr className='headerRow'>
+              <th>Number</th>
+              <th>Name</th>
+              <th>Piece Count</th>
+              <th>Instructions</th>
+            </tr>
+          </thead>
+          <tbody>
+            { rows.map((row, i) =>
+              <tr className='row' key={i}>
+                <td>{row.number}</td>
+                <td>{row.name}</td>
+                <td>{row.pieceCount}</td>
+                <td>
+                  <a
+                    className='instructionLink'
+                    onClick={() => openLinks(row.links)}
+                  >Open</a>
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+        <Dialog
+          open={Boolean(multiLinkModalTarget)}
+          onClose={() => setMultiLinkModalTarget(null)}
+        >
+          <Paper sx={{ padding: 1 }}>
+            <Typography>
+              This set contains multiple instructions. Which would you like to open?
+            </Typography>
+            { multiLinkModalTarget?.length > 0 && multiLinkModalTarget.map((link, i) =>
+                <Typography
+                  onClick={() => {
+                    setLinkTarget(link);
+                    setMultiLinkModalTarget(null);
+                  }}
+                  sx={{ cursor: 'pointer', color: '#8fd3fe' }}
+                  key={i}
+                >
+                  {link}
+                </Typography>
+            )}
+          </Paper>
+        </Dialog>
+        <Dialog
+          open={Boolean(linkTarget)}
+          onClose={() => setLinkTarget(null)}
+          maxWidth='xl'
+        >
+          <Paper>
+            <iframe src={linkTarget} style={{ border: 'none', width: '100vw', height: '100vh' }} />
+          </Paper>
+        </Dialog>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/instructions.js
@@ -0,0 +1,69 @@
+export const instructions = [
+  {
+    number: 30211,
+    name: 'Uruk-Hai with Ballista',
+    pieceCount: 21,
+    theme: 'Lord of the Rings, the Hobbit',
+    links: [
+      'https://www.bricklink.com/myImg/740413.jpg',
+    ],
+  },
+  {
+    number: 9474,
+    name: `The Battle of Helm's Deep`,
+    pieceCount: 1368,
+    theme: 'Lord of the Rings, the Hobbit',
+    links: [
+      'https://www.lego.com/cdn/product-assets/product.bi.core.pdf/6021624.pdf',
+      'https://www.lego.com/cdn/product-assets/product.bi.core.pdf/6021625.pdf',
+      'https://www.lego.com/cdn/product-assets/product.bi.core.pdf/6021626.pdf',
+      'https://www.lego.com/cdn/product-assets/product.bi.core.pdf/6021628.pdf',
+    ],
+  },
+  {
+    number: 30212,
+    name: 'Mirkwood Elf Guard',
+    pieceCount: 27,
+    theme: 'Lord of the Rings, the Hobbit',
+    links: [
+      'https://img.bricklink.com/ItemImage/IN/0/30212-1.png',
+    ],
+  },
+  {
+    number: 30216,
+    name: 'Lake Town Guard',
+    pieceCount: 27,
+    theme: 'Lord of the Rings, the Hobbit',
+    links: [
+      'https://img.bricklink.com/ItemImage/IN/0/30216-1.png',
+    ],
+  },
+  {
+    number: 30210,
+    name: 'Frodo with Cooking Corner',
+    pieceCount: 29,
+    theme: 'Lord of the Rings, the Hobbit',
+    links: [
+      'https://img.bricklink.com/ItemImage/IN/0/30210-1.png',
+    ],
+  },
+  {
+    number: 30213,
+    name: 'Gandalf at Dol Goldur',
+    pieceCount: 25,
+    theme: 'Lord of the Rings, the Hobbit',
+    links: [
+      'https://img.bricklink.com/ItemImage/IN/0/30213-1.png',
+    ],
+  },
+  {
+    number: 30215,
+    name: 'Legolas Greenleaf',
+    pieceCount: 29,
+    theme: 'Lord of the Rings, the Hobbit',
+    links: [
+      'https://media.brickinstructions.com/30000/30215/001.jpg',
+      'https://media.brickinstructions.com/30000/30215/002.jpg',
+    ],
+  },
+];
